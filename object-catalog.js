5efe7ab6c7d9f26e7edc2e754c10a447f9e2d88e
millionmind15-cpu <millionmind15@gmail.com>
2026-05-08 13:59:40 +0200

    Add files via upload

---
+++ object-catalog.js
@@ -0,0 +1,74 @@
+window.IMMOMEDIA_OBJECTS = [
+  {
+    id: 'OBJ-PARKSIDE-LERCHENAUER-FELD',
+    name: 'PARKSIDE LERCHENAUER FELD',
+    location: 'München, Lerchenauer Feld',
+    status: 'Vertriebsstart',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  },
+  {
+    id: 'OBJ-FUTURIA-GARCHING',
+    name: 'FUTURIA GARCHING',
+    location: 'Garching bei München',
+    status: 'Im Hochbau',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  },
+  {
+    id: 'OBJ-ZWEIKLANG-BAD-TOELZ',
+    name: 'ZWEIKLANG BAD TÖLZ',
+    location: 'Bad Tölz',
+    status: 'Bezugsfertig',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  },
+  {
+    id: 'OBJ-THE-VIEW-BAD-AIBLING',
+    name: 'THE VIEW BAD AIBLING',
+    location: 'Bad Aibling',
+    status: 'Bezugsfertig',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  },
+  {
+    id: 'OBJ-AMPERLIVING-OLCHING',
+    name: 'AMPERLIVING OLCHING',
+    location: 'Olching',
+    status: 'Bezugsfertig',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  },
+  {
+    id: 'OBJ-IMPULS-HADERN',
+    name: 'IMPULS HADERN',
+    location: 'München, Hadern',
+    status: '28 Neubauwohnungen',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  },
+  {
+    id: 'OBJ-THERESE-M-MAXVORSTADT',
+    name: 'THERESE, M-MAXVORSTADT',
+    location: 'München, Maxvorstadt',
+    status: '112 Wohnungen',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  },
+  {
+    id: 'OBJ-HERZSTUECK-GARMISCH',
+    name: 'HERZSTÜCK GARMISCH',
+    location: 'Garmisch-Partenkirchen',
+    status: '148 Wohnungen',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  },
+  {
+    id: 'OBJ-PARKSIDE-RIEM',
+    name: 'PARKSIDE RIEM',
+    location: 'München, Riem',
+    status: '68 Wohneinheiten',
+    category: 'Aktuelles Objekt',
+    formActive: true
+  }
+];
